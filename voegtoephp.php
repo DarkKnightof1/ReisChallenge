07fd8c422af73b3564b3fca3f61e5729ac283dda
DarkKnightof1 <mischa@boskat.eu>
2019-12-10 08:32:18 +0100

    volgens zet database op met nieuwe manier

---
+++ voegtoephp.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: misch
+ * Date: 01-12-2019
+ * Time: 13:16
+ */
+
+//$dbh = new PDO('mysql:host=localhost;dbname=boodschappen2;port=8889', 'root', 'root');
+//$boodschappen = $dbh->query('SELECT * from product');
+//foreach ($boodschappen as $boodschap) {
+// echo "<li>".$boodschap{'omschrijving'}."</li>";
+//}
+
+//if(isset($_GET{'idboodschap'})) {
+//$idproduct = $_GET{'idproduct'};
+//$omschrijving = $_GET{'omschrijving'};
+// $sql = "insert into boodschappen2 (idproduct, omschrijving) values ($idproduct, '$omschrijving')";
+//$dbh->query($sql);
+//}
+
+//fix insert//
